518347d4e217c5fbb0358200dfa76335e7096282
Daniel Bel <danielbel12124@gmail.com>
2023-01-25 00:22:50 +0200

    yey

---
+++ main.py
@@ -0,0 +1,15 @@
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        if str(message.author.id) == "273745908137459712":
+            await message.channel.send('Shut up nigga!')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('my token goes here')
