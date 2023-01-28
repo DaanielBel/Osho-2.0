3fee4a62e316a4cc86a4e6a34154b1df94252a93
Daniel Bel <danielbel12124@gmail.com>
2023-01-28 15:49:57 +0200

    asdasd

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class MyClient(discord.Client):
     async def on_voice_state_update(self, member, before, after):
         if not member.bot:
             print(member.name + " joined " + after.channel.name)
-            await self.join_voice_channel(after.channel.id)
+            await self.connect(after.channel)
 
 intents = discord.Intents.default()
 intents.message_content = True
