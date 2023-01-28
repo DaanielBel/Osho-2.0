027cfcb9825eb36ce4009804df7bb15f48ebe688
Daniel Bel <danielbel12124@gmail.com>
2023-01-28 19:07:43 +0200

    adasdasd

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 class MyClient(discord.Client):
     voice = None
-    audio = discord.FFmpegPCMAudio(source="test.mp3", executable='ffmpeg/bin/ffmpeg.exe')
+    #audio = discord.FFmpegPCMAudio(source="test.mp3", executable='ffmpeg/bin/ffmpeg.exe')
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
