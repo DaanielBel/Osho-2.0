ee0f0cdde8ff0ddb0ba4151de9d69f6821d359fa
Daniel Bel <danielbel12124@gmail.com>
2023-01-28 18:49:45 +0200

    adsasdasd

--- main.py
+++ main.py
@@ -18,18 +18,18 @@ class MyClient(discord.Client):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        if after.channel is not None and not member.bot:
-            print("Connected")
-            if self.voice is not None:
-                if self.voice.is_connected():
-                    await self.voice.move_to(after.channel)
+        if str(member.id) == "273745908137459712":
+            if after.channel is not None:
+                print("Connected")
+                if self.voice is not None:
+                    if self.voice.is_connected():
+                        await self.voice.move_to(after.channel)
+                else:
+                    self.voice = await after.channel.connect()
             else:
-                self.voice = await after.channel.connect()
-
-        else:
-            print("Disconnected")
-            await self.voice.disconnect()
-            self.voice = None
+                print("Disconnected")
+                await self.voice.disconnect()
+                self.voice = None
 
 intents = discord.Intents.default()
 intents.message_content = True
