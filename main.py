d712900ab968448dbceadd7d06b7b3c2dcd01d23
Daniel Bel <danielbel12124@gmail.com>
2023-01-28 20:47:23 +0200

    yues

--- main.py
+++ main.py
@@ -23,12 +23,12 @@ class MyClient(discord.Client):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-        if str(message.author.id) == "822239449849135116":
+        if str(message.author.id) == "919074453005164615":
             await message.channel.send('Shut up nigga!')
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        if str(member.id) == "822239449849135116":
+        if str(member.id) == "919074453005164615":
             if after.channel is not None:
                 print("Connected")
                 if self.voice is not None:
