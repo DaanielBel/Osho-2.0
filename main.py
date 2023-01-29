21e45b294339ce7978649cd5029d1592e5e2a08d
Daniel Bel <danielbel12124@gmail.com>
2023-01-29 23:55:21 +0200

    dasda

--- main.py
+++ main.py
@@ -10,44 +10,47 @@ from discord.ext import commands
 
 load_dotenv()
 
+voice = [{885615716915642498 : None}]
+target = [{885615716915642498 : None}]
+
 intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True
-client = commands.Bot(intents=intents, command_prefix = "$")
+intents.guilds = True
+
+client = commands.Bot(intents=intents, command_prefix = "$$")
 
 def discon(bot, member):
     member.edit(voice_channel=None)
     bot.voice.disconnect()
     bot.voice = None
 
-voice = None
-target = [{885615716915642498 : 318475929221332992}]
+@client.event
+async def on_ready():
+    print(f'Logged on as {client}!')
 
 @client.event
-async def on_ready(self):
-    print(f'Logged on as {self.user}!')
-
-@client.event
-async def on_message(self, message):
-    if str(message.author.id) == self.target:
+async def on_message(ctx, message):
+    if message.author.id == "822239449849135116":
         await message.channel.send('You stupid nigga!')
 
-@client.command()
-async def target(self, tar: discord.member):
-    print("target")
-    self.target = str(tar.id)
-
+@client.event
+async def on_guild_join(guild):
+    voice.append({guild.id : None})
+    target.append({guild.id : None})
 
 @commands.Cog.listener()
 async def on_voice_state_update(self, member, before, after):
-    if member.id == target(member.guild.id):
+    if str(member.id) == "822239449849135116":
         if after.channel is not None:
             print("Connected")
             if self.voice is not None:
                 if self.voice.is_connected():
-                    await self.voice.move_to(after.channel)     
+                    await self.voice.move_to(after.channel)
+                    
             else:
                 self.voice = await after.channel.connect()
+
 
             self.voice.play(discord.FFmpegPCMAudio("youStupid.mp3"))
             await asyncio.sleep(1.5)
@@ -59,7 +62,14 @@ async def on_voice_state_update(self, member, before, after):
             await self.voice.disconnect()
             self.voice = None
 
+
+@client.commands()
+async def target(ctx, tar : discord.member):
+    await print(tar)
+    await ctx.channel.send(tar)
+
+
 async def main():
-    await client.start(os.environ['TOKEN'])
+    client.run(os.environ['TOKEN'])
 
 asyncio.run(main())
